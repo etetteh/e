5d3624deaffd455ff389be631d8b52d6e5a4a723
Enoch Tetteh <cutetetteh@gmail.com>
2023-02-13 10:51:50 -0500

    Updated script to run inference on multiple images input plus some minor refactoring

--- deep_learning/image_classification/inference.py
+++ deep_learning/image_classification/inference.py
@@ -6,71 +6,65 @@ import warnings
 import onnxruntime
 import torch.nn.functional as f
 from PIL import Image
-import numpy as np
 import torchvision.transforms as transforms
 
 import utils
 
 
-def run_one_inference(onnx_model_path: str, img_path: str, dataset_dir_or_classes_file: str) -> dict:
+def run_inference(onnx_model_path: str, imgs_path: str, dataset_dir_or_classes_file: str) -> list:
     """
     Runs one inference on a given ONNX model and image.
 
     Parameters:
         - onnx_model_path (str): Path to the ONNX model.
-        - img_path (str): Path to the image.
+        - imgs_paths (str): Paths to the images.
         - dataset_dir_or_classes_file (str): Path to the directory containing the dataset classes or Path to a text file
                                 containing class names (each class name on a separate line).
 
     Returns:
-        - dict: A dictionary containing the predicted label and the associated probability.
+        - dict: A list of dictionary/dictionaries containing the predicted label and the associated probability.
 
     """
 
-    def to_numpy(tensor: torch.Tensor) -> np.ndarray:
-        """
-        Convert a PyTorch tensor to a numpy array.
-
-        Parameters:
-            - tensor (torch.Tensor): The input tensor.
-
-        Returns:
-            - np.ndarray: The converted numpy array.
-        """
-        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
-
     if os.path.isfile(dataset_dir_or_classes_file):
-        with open(dataset_dir_or_classes_file, "r") as file:
-            classes = sorted(file.read().splitlines())
+        with open(dataset_dir_or_classes_file, "r") as file_in:
+            classes = sorted(file_in.read().splitlines())
     else:
         classes = utils.get_classes(dataset_dir_or_classes_file)
 
-    # Preprocess the image
     transform_img = transforms.Compose([transforms.Resize([224, 224]), transforms.ToTensor()])
-    img = Image.open(img_path)
-    img = transform_img(img)
-    img.unsqueeze_(0)
 
-    # Load the ONNX model and run the inference
+    # if
+    imgs = [Image.open(img_path) for img_path in imgs_path]
+    imgs = [transform_img(img) for img in imgs]
+    imgs = [img.unsqueeze_(0) for img in imgs]
+
     ort_session = onnxruntime.InferenceSession(onnx_model_path)
-    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img)}
-    ort_outs = ort_session.run(None, ort_inputs)
 
-    # Get the prediction and its probability
-    prob = f.softmax(torch.from_numpy(ort_outs[0]), dim=1)
-    top_p, top_class = torch.topk(prob, 1, dim=1)
+    results = []
+    for i, img in enumerate(imgs):
+        ort_inputs = {ort_session.get_inputs()[0].name: utils.convert_tensor_to_numpy(img)}
+        ort_outs = ort_session.run(None, ort_inputs)
 
-    return {
-        "Predicted Label": classes[top_class.item()],
-        "Probability": round(top_p.item() * 100, 2),
-    }
+        prob = f.softmax(torch.from_numpy(ort_outs[0]), dim=1)
+        top_p, top_class = torch.topk(prob, 1, dim=1)
+
+        results.append({
+            f"image {i}": {
+                "Predicted Label": classes[top_class.item()],
+                "Probability": round(top_p.item() * 100, 2),
+            }
+        }
+        )
+
+    return results
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Run a single inference on a ONNX model for image classification")
 
     parser.add_argument("--onnx_model_path", type=str, required=True, help="Path to the ONNX model")
-    parser.add_argument("--img_path", type=str, required=True, help="Path to the image to be classified")
+    parser.add_argument("--imgs_paths", nargs="*", required=True, help="Path to the image to be classified")
     parser.add_argument("--dataset_dir_or_classes_file", type=str, required=True,
             help="Path to the directory containing the dataset classes or Path to a text file containing class names")
 
@@ -82,5 +76,5 @@ if __name__ == "__main__":
 
     args = get_args()
 
-    result = run_one_inference(args.onnx_model_path, args.img_path, args.dataset_dir_or_classes_file)
+    result = run_inference(args.onnx_model_path, args.imgs_paths, args.dataset_dir_or_classes_file)
     print(result)
