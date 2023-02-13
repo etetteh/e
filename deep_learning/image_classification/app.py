7b474ebebeafacdcd4288f8764827eb803ec2011
Enoch Tetteh <cutetetteh@gmail.com>
2023-02-13 10:54:38 -0500

    Updated script to import code from existing scripts

--- deep_learning/image_classification/app.py
+++ deep_learning/image_classification/app.py
@@ -1,33 +1,9 @@
 import json
-import os
 import uvicorn
-
-import numpy as np
-import torch
-import torch.nn.functional as f
-import torchvision.transforms as transforms
-from PIL import Image
-
-import onnxruntime
-import utils
-
 from fastapi import FastAPI, UploadFile
-
+from inference import run_inference
 
 app = FastAPI()
-
-
-def to_numpy(tensor: torch.Tensor) -> np.ndarray:
-    """
-    Convert tensor to numpy ndarray
-
-    Parameters:
-        - tensor (torch.Tensor): torch tensor to be converted
-
-    Returns:
-        - np.ndarray: numpy ndarray converted from tensor
-    """
-    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
 
 @app.post("/predict")
@@ -38,48 +14,21 @@ async def predict(file: UploadFile):
     Parameters:
         - file (UploadFile): a JSON file containing the following keys:
         - onnx_model_path (str): Path to ONNX model
-        - img_path (str): Path to input image
+        - imgs_paths (str): Paths to the input images
         - dataset_dir_or_classes_file (str): Path to dataset directory or file with list of classes
 
     Returns:
-        - dict: A dictionary containing the following keys:
-            "Predicted Label": str, the predicted label of the image
-            "Probability": float, the confidence of the prediction
+        - List[dict]: A list of dictionary/dictionaries containing the following keys:
+            image:
+                "Predicted Label": str, the predicted label of the image
+                "Probability": float, the confidence of the prediction
     """
     input_json = json.loads(await file.read())
     onnx_model_path = input_json['onnx_model_path']
-    img_path = input_json['img_paths']
+    imgs_path = input_json['imgs_paths']
     dataset_dir_or_classes_file = input_json['dataset_dir_or_classes_file']
 
-    if os.path.isfile(dataset_dir_or_classes_file):
-        with open(dataset_dir_or_classes_file, "r") as file_in:
-            classes = sorted(file_in.read().splitlines())
-    else:
-        classes = utils.get_classes(dataset_dir_or_classes_file)
-
-    transform_img = transforms.Compose([transforms.Resize([224, 224]), transforms.ToTensor()])
-
-    imgs = [Image.open(img_path) for img_path in img_path]
-    imgs = [transform_img(img) for img in imgs]
-    imgs = [img.unsqueeze_(0) for img in imgs]
-
-    ort_session = onnxruntime.InferenceSession(onnx_model_path)
-
-    results = []
-    for i, img in enumerate(imgs):
-        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img)}
-        ort_outs = ort_session.run(None, ort_inputs)
-
-        prob = f.softmax(torch.from_numpy(ort_outs[0]), dim=1)
-        top_p, top_class = torch.topk(prob, 1, dim=1)
-
-        results.append({
-            f"image {i}": {
-                "Predicted Label": classes[top_class.item()],
-                "Probability": round(top_p.item() * 100, 2),
-            }
-        }
-        )
+    results = run_inference(onnx_model_path, imgs_path, dataset_dir_or_classes_file)
 
     return results
 
